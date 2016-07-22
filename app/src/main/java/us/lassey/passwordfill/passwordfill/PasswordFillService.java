12b8b0f39918a19cf5881844d6395b1cfbe283c7
Brad Lassey <brad@lassey.us>
2016-07-22 13:49:20 -0400

    fills in user name and password

--- app/src/main/java/us/lassey/passwordfill/passwordfill/PasswordFillService.java
+++ app/src/main/java/us/lassey/passwordfill/passwordfill/PasswordFillService.java
@@ -2,6 +2,8 @@ package us.lassey.passwordfill.passwordfill;
 
 import android.accessibilityservice.AccessibilityService;
 import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.CursorLoader;
 import android.content.DialogInterface;
@@ -9,15 +11,26 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.content.CursorLoader;
 import android.database.CursorWrapper;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class PasswordFillService extends AccessibilityService {
     String PASSWORD_URI = "content://org.mozilla.fennec_blassey.db.passwords/passwords";
@@ -28,27 +41,19 @@ public class PasswordFillService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         // get the source node of the event
-        AccessibilityNodeInfo nodeInfo = event.getSource();
+        final AccessibilityNodeInfo nodeInfo = event.getSource();
         if (nodeInfo.isPassword()) {
-            Log.d("PasswordFill", "Got a password for: " + event);
             String packagename = event.getPackageName().toString();
             String[] nameParts = packagename.split("\\.");
             String[] host = {"%" + nameParts[1] + "." + nameParts[0] + "%"};
-            Log.i("PasswordFill", "looking for " + host[0]);
             Uri uri = Uri.parse(PASSWORD_URI);
             String[] proj = {"hostname", "encryptedUsername", "encryptedPassword", "id"};
-            Cursor c = this.getContentResolver().query(uri, proj, "hostname LIKE ?", host, null);
-            if (c != null) {
-                Log.i("PasswordFill", "got " + c.getCount() + " rows");
-                if (c.getCount() > 0) {
-                    c.moveToFirst();
-                    String[] cols = c.getColumnNames();
-                    for (int i = 0; i < cols.length; i++) {
-                        Log.i("PasswordFill", cols[i] + " : " + c.getString(i));
-                    }
-                    //CursorLoader loader = new CursorLoader(this, uri, null, "hostname LIKE ?", host, null);
+            Cursor cursor = this.getContentResolver().query(uri, proj, "hostname LIKE ?", host, null);
+            if (cursor != null) {
+                if (cursor.getCount() > 0) {
+                    final ClipboardManager clipboard = (ClipboardManager)this.getSystemService(Context.CLIPBOARD_SERVICE);
                     int[] views = {R.id.hostname, R.id.username};
-                    CursorWrapper cw = new CursorWrapper(c) {
+                    CursorWrapper cw = new CursorWrapper(cursor) {
                         @Override
                         public int getColumnIndex (String columnName) {
                             if (columnName.equals("_id")) {
@@ -65,17 +70,75 @@ public class PasswordFillService extends AccessibilityService {
                             return getWrappedCursor().getColumnIndexOrThrow(columnName);
                         }
                     };
-                    SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.layout_login, cw, proj, views, 0 );
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    final SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.layout_login, cw, proj, views, 0 );
+                    AlertDialog.Builder builderSingle = new AlertDialog.Builder(this);
+                    builderSingle.setTitle("Login with your saved Firefox password?");
+                    builderSingle.setNegativeButton(
+                            "cancel",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
 
-                    builder.setView(R.layout.password_fill_dialog_layout);
-                    AlertDialog alert = builder.create();
+                    builderSingle.setPositiveButton(
+                            "OK",
+                            new DialogInterface.OnClickListener() {
+                                private void setText(AccessibilityNodeInfo node, String text) {
+                                    Bundle arguments = new Bundle();
+                                    arguments.putCharSequence(AccessibilityNodeInfo
+                                            .ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, text);
+                                    boolean success = node.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments);
+                                    if (!success) {
+                                        ClipData oldClip = clipboard.getPrimaryClip();
+                                        ClipData clip = ClipData.newPlainText("password", "testing");
+                                        clipboard.setPrimaryClip(clip);
+                                        success = node.performAction(AccessibilityNodeInfo.ACTION_PASTE);
+                                        clipboard.setPrimaryClip(oldClip);
+                                    }
+                                }
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    AlertDialog alert = (AlertDialog)dialog;
+                                    Integer tag = (Integer)alert.getButton(DialogInterface.BUTTON_POSITIVE).getTag();
+                                    Cursor c = (Cursor)adapter.getItem(tag.intValue());
+                                    final String password = c.getString(c.getColumnIndex("encryptedPassword"));
+                                    final String username =  c.getString(c.getColumnIndex("encryptedUsername"));
+                                    Handler handler = new Handler();
+                                    // Need to let the dialog dismiss before we can insert text
+                                    final Runnable runnable = new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            setText(nodeInfo, password);
+                                            AccessibilityNodeInfo user = nodeInfo.focusSearch(View.FOCUS_UP);
+                                            setText(user,username);
+                                        }
+                                    };
+                                    handler.postDelayed(runnable,100);
+                                }
+                            });
+
+                    //builderSingle.setAdapter(
+                    builderSingle.setSingleChoiceItems(
+                            adapter, 0,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Button ok = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_POSITIVE);
+                                    ok.setTag(new Integer(which));
+                                    ok.setEnabled(true);
+                                    Log.i("PAsswordFill", " you selected " + which);
+                                }
+                            });
+
+                    final AlertDialog alert = builderSingle.create();
+                    alert.getListView().setSelector(new ColorDrawable(Color.argb(63,255,195,63)));
+
                     alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-
                     alert.show();
-                    ListView listView = (ListView)alert.findViewById(R.id.login_listView);
-                    listView.setAdapter(adapter);
-
+                    alert.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
                 }
             }
         }
